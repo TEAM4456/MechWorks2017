bfa24b4961fe3f4bb7690ab80e5ad7753c11c314
Tyrov <dakingxvi@gmail.com>
2017-01-21 11:05:03 -0500

    added Drive and preliminary code for Drive

---
+++ src/main/java/org/usfirst/frc/team4456/subsystems/Drive.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team4456.subsystems;
+
+import org.usfirst.frc.team4456.subsystems.*;
+import org.usfirst.frc.team4456.commands.*;
+import org.usfirst.frc.team4456.RobotMap;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import com.ctre.CANTalon;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drive extends Subsystem {
+	
+	//public final CANTalon leftDrive = RobotMap.leftDrive;
+	//public final CANTalon rightDrive = RobotMap.rightDrive;
+	//public final RobotDrive robotDrive = RobotMap.robotDrive;
+	
+	
+	protected void initDefaultCommand() {
+		// set default command here
+	}
+	
+	public void drive(Joystick joystick) {
+		
+		// robotDrive.setSafetyEnabled(false);
+		
+	}
+	
+}
