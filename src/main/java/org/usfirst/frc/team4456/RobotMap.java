af90f82bbed207f5cce01077ad55a342cc9a21cc
Brian Ben Maranville <brian.maranville@nist.gov>
2017-01-21 16:07:13 -0500

    adding code to make follower motors for master drive Talons

--- src/main/java/org/usfirst/frc/team4456/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4456/RobotMap.java
@@ -18,9 +18,21 @@ public class RobotMap {
 	public static void init() {
 		
 		leftDriveTalon1 = new CANTalon(3);
+		//leftDriveTalon2 = new CANTalon(5);
+		//leftDriveTalon2.changeControlMode(CANTalon.TalonControlMode.Follower);
+		//leftDriveTalon2.set(leftDriveTalon1.getDeviceID());
+		//// If the slave drive has to be run the opposite direction from the master, uncomment this:
+		//leftDriveTalon2.reverseOutput(true);
+
 		//LiveWindow.addActuator("Drive", "leftDrive", leftDriveTalon1);
 		
 		rightDriveTalon1 = new CANTalon(4);
+		//rightDriveTalon2 = new CANTalon(6);
+		//rightDriveTalon2.changeControlMode(CANTalon.TalonControlMode.Follower);
+		//rightDriveTalon2.set(rightDriveTalon1.getDeviceID());
+		//// If the slave drive has to be run the opposite direction from the master, uncomment this:
+        //rightDriveTalon2.reverseOutput(true);
+
 		//LiveWindow.addActuator("Drive", "rightDrive", rightDriveTalon1);
 		
 	}
