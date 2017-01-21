2f6137190cb473d207820b66dc733527280cabe4
Brian Ben Maranville <brian.maranville@nist.gov>
2017-01-21 15:35:16 -0500

    adding numbers for old robot drive motors

--- src/main/java/org/usfirst/frc/team4456/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4456/RobotMap.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team4456;
 
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 
 public class RobotMap {
 	
@@ -16,8 +18,13 @@ public class RobotMap {
 	
 	public RobotMap() {
 		
-		
-		
-	}
+		leftDriveTalon1 = new CANTalon(3);
+        LiveWindow.addActuator("Drive", "leftDrive", leftDriveTalon1);
+
+        rightDriveTalon1 = new CANTalon(4);
+        LiveWindow.addActuator("Drive", "rightDrive", rightDriveTalon1);
+
+
+    }
 	
 }
