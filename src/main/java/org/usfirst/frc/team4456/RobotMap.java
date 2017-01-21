1515c1fe5e8bbf52afe9b3914823c9084981846e
Tyrov <dakingxvi@gmail.com>
2017-01-21 15:14:22 -0500

    added tentative list to RobotMap

--- src/main/java/org/usfirst/frc/team4456/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4456/RobotMap.java
@@ -4,6 +4,14 @@ package org.usfirst.frc.team4456;
 
 public class RobotMap {
 	
-	
+	// leftDriveTalon1
+	// leftDriveTalon2 <-- slave to leftDriveTalon1
+	// rightDriveTalon1
+	// rightDriveTalon2 <-- slave to rightDriveTalon1
+	// loaderTalon
+	// shooterTalon
+	// winchTalon
+	// deflectorTalon
+	// intakeTalon
 	
 }
