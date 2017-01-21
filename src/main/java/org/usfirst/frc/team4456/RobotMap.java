f62500088662c47c05158281c02007d1ddfe98ac
Brian Ben Maranville <brian.maranville@nist.gov>
2017-01-21 15:42:57 -0500

    adding some robot mapping

--- src/main/java/org/usfirst/frc/team4456/RobotMap.java
+++ src/main/java/org/usfirst/frc/team4456/RobotMap.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4456;
 
 import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class RobotMap {
 	
@@ -13,5 +14,16 @@ public class RobotMap {
 	public static CANTalon winchTalon;
 	public static CANTalon deflectorTalon;
 	public static CANTalon intakeTalon;
+
+    public static void init() {
+
+        leftDriveTalon1 = new CANTalon(3);
+        LiveWindow.addActuator("Drive", "leftDrive", leftDriveTalon1);
+
+        rightDriveTalon1 = new CANTalon(4);
+        LiveWindow.addActuator("Drive", "rightDrive", rightDriveTalon1);
+
+
+    }
 	
 }
