479ca38cf507abf1ee286429743cd38ec80c34b0
Tyrov <dakingxvi@gmail.com>
2017-01-28 14:05:43 -0500

    added intake stuff

---
+++ src/main/java/org/usfirst/frc/team4456/commands/toggleIntake.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team4456.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class toggleIntake extends Command {
+	
+	public toggleIntake() {
+		
+	}
+	
+	protected void initialize() {
+		
+	}
+	
+	protected void execute() {
+		
+	}
+	
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+	
+	protected void end() {
+		
+	}
+	
+	protected void interrupted() {
+		end();
+	}
+}
--- src/main/java/org/usfirst/frc/team4456/subsystems/Intake.java
+++ src/main/java/org/usfirst/frc/team4456/subsystems/Intake.java
@@ -10,4 +10,6 @@ public class Intake extends Subsystem {
 		// set default command here
 	}
 	
+	
+	
 }
