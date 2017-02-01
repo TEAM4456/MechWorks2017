e1b1c64fcccc97a8a2029f8d00ec5ab8ef40e25c
Tyrov <dakingxvi@gmail.com>
2017-02-01 16:10:48 -0500

    added code for Shooter and toggleShooter

---
+++ src/main/java/org/usfirst/frc/team4456/commands/toggleShooter.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4456.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team4456.subsystems.Shooter;
+
+public class toggleShooter extends Command {
+	
+	boolean running;
+	
+	protected void initialize() {
+		running = false;
+	}
+	
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+	
+	public synchronized void start() {
+		running = true;
+		Shooter.startShooter();
+	}
+	
+	public synchronized void cancel() {
+		end();
+	}
+	
+	protected void end() {
+		Shooter.stopShooter();
+		running = false;
+	}
+	
+	protected void interrupted() {
+		end();
+	}
+	
+	public synchronized boolean isRunning() {
+		return running;
+	}
+	
+}
--- src/main/java/org/usfirst/frc/team4456/subsystems/Shooter.java
+++ src/main/java/org/usfirst/frc/team4456/subsystems/Shooter.java
@@ -2,12 +2,28 @@ package org.usfirst.frc.team4456.subsystems;
 
 // imports here
 
+import org.usfirst.frc.team4456.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends Subsystem {
 	
 	protected void initDefaultCommand() {
 		// set default command here
+	}
+	
+	public static void startShooter() {
+		
+		RobotMap.shooterTalon.set(SmartDashboard.getNumber("Shooter Speed", 0.01));
+		
+	}
+	
+	public static void stopShooter() {
+		
+		RobotMap.shooterTalon.set(0);
+		
 	}
 	
 }
