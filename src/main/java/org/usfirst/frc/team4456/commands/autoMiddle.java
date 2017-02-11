e8962f3058e8f73659e4c6302e62271a89f6445e
Tyrov <dakingxvi@gmail.com>
2017-02-11 15:00:49 -0500

    add autonomous commands with command structure

---
+++ src/main/java/org/usfirst/frc/team4456/commands/autoMiddle.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4456.commands;
+
+import org.usfirst.frc.team4456.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class autoMiddle extends Command {
+	
+	protected void initialize() {}
+	
+	protected boolean isFinished() { return isTimedOut(); }
+	
+	protected void execute() {}
+	
+	protected void end() {}
+	
+	protected void interrupted() { end(); }
+	
+}
