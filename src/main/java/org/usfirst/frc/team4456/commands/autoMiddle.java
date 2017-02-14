eaa0a9283647ed93ab9a89e66595a64ccb60b649
Tyrov <dakingxvi@gmail.com>
2017-02-14 15:40:24 -0500

    added preliminary (not working) code for autoMiddle

--- src/main/java/org/usfirst/frc/team4456/commands/autoMiddle.java
+++ src/main/java/org/usfirst/frc/team4456/commands/autoMiddle.java
@@ -2,13 +2,31 @@ package org.usfirst.frc.team4456.commands;
 
 import org.usfirst.frc.team4456.RobotMap;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class autoMiddle extends Command {
 	
+	CANTalon leftDriveTalon1 = RobotMap.leftDriveTalon1;
+	CANTalon rightDriveTalon1 = RobotMap.rightDriveTalon1;
+	
 	public autoMiddle() { /* requires(...) */ }
 	
-	protected void initialize() {}
+	protected void initialize() {
+		
+		leftDriveTalon1.changeControlMode(CANTalon.TalonControlMode.Position);
+		leftDriveTalon1.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		leftDriveTalon1.setPosition(0);
+		
+		rightDriveTalon1.changeControlMode(CANTalon.TalonControlMode.Position);
+		rightDriveTalon1.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+		rightDriveTalon1.setPosition(0);
+		
+		leftDriveTalon1.set(leftDriveTalon1.getPosition() + 2500);
+		rightDriveTalon1.set(rightDriveTalon1.getPosition() + 2500);
+		
+	}
 	
 	protected boolean isFinished() { return isTimedOut(); }
 	
