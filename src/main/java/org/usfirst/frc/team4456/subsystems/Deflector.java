6d3ea70f2a6cacab271b8c8eb712a1f3efc40968
Tyrov <dakingxvi@gmail.com>
2017-02-16 17:42:47 -0500

    changed comment from placeholder to info

--- src/main/java/org/usfirst/frc/team4456/subsystems/Deflector.java
+++ src/main/java/org/usfirst/frc/team4456/subsystems/Deflector.java
@@ -1,14 +1,13 @@
 package org.usfirst.frc.team4456.subsystems;
 
 import org.usfirst.frc.team4456.RobotMap;
-import org.usfirst.frc.team4456.commands.homeDeflector;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Deflector extends Subsystem {
 	
-	protected void initDefaultCommand() { /*setDefaultCommand(new homeDeflector());*/ }
+	protected void initDefaultCommand() { /* set default command here */ }
 	
 	public static void raiseDeflector() {
 		RobotMap.deflectorTalon.set(RobotMap.deflectorTalon.get() +
