7f0ae85a6d06b7479b0b77f9ed69849a263c945c
Tyrov <dakingxvi@gmail.com>
2017-02-18 16:28:57 -0500

    updated RobotMap_new

--- src/main/java/org/usfirst/frc/team4456/RobotMap_new.java
+++ src/main/java/org/usfirst/frc/team4456/RobotMap_new.java
@@ -1,7 +1,8 @@
 package org.usfirst.frc.team4456;
 
 import com.ctre.CANTalon;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.SerialPort;
 
 public class RobotMap_new {
 	
@@ -9,11 +10,13 @@ public class RobotMap_new {
 	public static CANTalon leftDriveTalon2; // <-- slave to leftDriveTalon1
 	public static CANTalon rightDriveTalon1;
 	public static CANTalon rightDriveTalon2; // <-- slave to rightDriveTalon2
-	public static CANTalon loaderTalon;
 	public static CANTalon shooterTalon;
 	public static CANTalon winchTalon;
 	public static CANTalon deflectorTalon;
 	public static CANTalon intakeTalon;
+	public static CANTalon agitatorTalon;
+	public static SerialPort lidarSerial;
+	public static DigitalInput deflectorSwitch;
 	
 	public static void init() {
 		
@@ -21,57 +24,37 @@ public class RobotMap_new {
 		leftDriveTalon2 = new CANTalon(4);
 		leftDriveTalon2.changeControlMode(CANTalon.TalonControlMode.Follower);
 		leftDriveTalon2.set(leftDriveTalon1.getDeviceID());
-		//// If the slave drive has to be run the opposite direction from the master, uncomment this:
-		//leftDriveTalon2.reverseOutput(true);
-		
-		//SmartDashboard.putData("leftDrive", leftDriveTalon1);
 		
 		rightDriveTalon1 = new CANTalon(1);
 		rightDriveTalon2 = new CANTalon(2);
 		rightDriveTalon2.changeControlMode(CANTalon.TalonControlMode.Follower);
 		rightDriveTalon2.set(rightDriveTalon1.getDeviceID());
-		//// If the slave drive has to be run the opposite direction from the master, uncomment this:
-		//rightDriveTalon2.reverseOutput(true);
 		
-		//SmartDashboard.putData("rightDrive", rightDriveTalon1);
-		
-		// **loaderTalon maybe not needed**
-		//loaderTalon = new CANTalon(1337); // PLACEHOLDER
-		//loaderTalon.changeControlMode(CANTalon.TalonControlMode.Position);
-		//loaderTalon.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
-		//loaderTalon.setPosition(0);
-		
-		//SmartDashboard.putData("loaderTalon", loaderTalon);
-		
-		shooterTalon = new CANTalon(5);
-		//shooterTalon.changeControlMode(CANTalon.TalonControlMode.Position);
-		//shooterTalon.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
-		//shooterTalon.setPosition(0);
-		
-		//SmartDashboard.putData("shooterTalon", shooterTalon);
+		shooterTalon = new CANTalon(3);
+		shooterTalon.changeControlMode(CANTalon.TalonControlMode.Speed);
+		shooterTalon.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+		shooterTalon.setPID(.7, 0.000001, 0);
+		shooterTalon.setInverted(true);
 		
 		winchTalon = new CANTalon(8);
-		//winchTalon.changeControlMode(CANTalon.TalonControlMode.Position);
-		//winchTalon.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
-		//winchTalon.setPosition(0);
 		
-		//SmartDashboard.putData("winchTalon", winchTalon);
+		deflectorTalon = new CANTalon(7);
+		deflectorTalon.changeControlMode(CANTalon.TalonControlMode.Position);
+		deflectorTalon.setFeedbackDevice(CANTalon.FeedbackDevice.CtreMagEncoder_Relative);
+		deflectorTalon.reverseSensor(true);
+		deflectorTalon.setPID(1.5, 0, 0);
 		
-		//deflectorTalon = new CANTalon(1337); // PLACEHOLDER
-		//deflectorTalon.changeControlMode(CANTalon.TalonControlMode.Position);
-		//deflectorTalon.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
-		//deflectorTalon.setPosition(0);
+		intakeTalon = new CANTalon(5);
 		
-		//SmartDashboard.putData("deflectorTalon", deflectorTalon);
+		agitatorTalon = new CANTalon(9);
+		agitatorTalon.changeControlMode(CANTalon.TalonControlMode.Voltage);
+		agitatorTalon.setVoltageCompensationRampRate(0);
+		agitatorTalon.setSafetyEnabled(false);
 		
-		intakeTalon = new CANTalon(3);
-		//intakeTalon.changeControlMode(CANTalon.TalonControlMode.Position);
-		//intakeTalon.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
-		//intakeTalon.setPosition(0);
+		lidarSerial = new SerialPort(9600, SerialPort.Port.kUSB);
 		
-		//SmartDashboard.putData("intakeTalon", intakeTalon);
+		deflectorSwitch = new DigitalInput(0);
 		
 	}
 	
 }
-
