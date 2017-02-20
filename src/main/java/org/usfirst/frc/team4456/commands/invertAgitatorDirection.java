5b70e533e15e073057b4981153637f8682fd437a
Tyrov <dakingxvi@gmail.com>
2017-02-20 14:05:21 -0500

    added invertAgitatorDirection

---
+++ src/main/java/org/usfirst/frc/team4456/commands/invertAgitatorDirection.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team4456.commands;
+
+import org.usfirst.frc.team4456.Robot;
+import org.usfirst.frc.team4456.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class invertAgitatorDirection extends Command {
+	
+	boolean finished;
+	
+	public invertAgitatorDirection() { requires(Robot.deflector); }
+	
+	protected void initialize() { finished = false; }
+	
+	protected boolean isFinished() { return !finished; }
+	
+	protected void execute() {
+		RobotMap.agitatorTalon.reverseOutput(true);
+		finished = true;
+	}
+	
+	protected void end() { RobotMap.agitatorTalon.reverseOutput(false); }
+	
+	protected void interrupted() { end(); }
+	
+}
